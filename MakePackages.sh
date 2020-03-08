04bff38c4238af17c85f5a519096cb2e5cc994e4
Dan <danielmi@unity3d.com>
2020-03-07 16:16:30 -0800

    added zip and tar.gz functionality to shell script

--- MakePackages.sh
+++ MakePackages.sh
@@ -9,5 +9,12 @@ UX_FOLDER="Assets/UX"
 IMAGE_FOLDER="Assets/ImageTracking"
 UX_PACKAGE=UX.unitypackage
 IMAGE_PACKAGE=ImageTracking.unitypackage
+
 $UNITY_PATH -batchmode -nographics -quit -projectPath $PROJECT_PATH -exportPackage $COMMON_FOLDER $UX_FOLDER "$PACKAGE_PATH$UX_PACKAGE"
 $UNITY_PATH -batchmode -nographics -quit -projectPath $PROJECT_PATH -exportPackage $COMMON_FOLDER $IMAGE_FOLDER "$PACKAGE_PATH$IMAGE_PACKAGE"
+
+echo "Making Zip..."
+cd $PROJECT_PATH; zip -r source.zip Assets/ Packages/ ProjectSettings/
+mv source.zip $PACKAGE_PATH
+tar -zcvf source.tar.gz Assets/ Packages/ ProjectSettings/
+mv source.tar.gz $PACKAGE_PATH
