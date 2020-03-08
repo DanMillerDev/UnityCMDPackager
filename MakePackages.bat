d2d25c4f060d2586481e6010dcef2f06b222db99
Dan Miller <danmiller126407@gmail.com>
2020-03-07 20:56:40 -0800

    added zipping and tar.gz to batch file

--- MakePackages.bat
+++ MakePackages.bat
@@ -10,4 +10,11 @@ set UX_PACKAGE=UX.unitypackage
 set IMAGE_PACKAGE=ImageTracking.unitypackage
 %UNITY_PATH% -batchmode -nographics -quit -projectPath %PROJECT_PATH% -exportPackage %COMMON_FOLDER% %UX_FOLDER% "%PACKAGE_PATH%%UX_PACKAGE%"
 %UNITY_PATH% -batchmode -nographics -quit -projectPath %PROJECT_PATH% -exportPackage %COMMON_FOLDER% %IMAGE_FOLDER% "%PACKAGE_PATH%%IMAGE_PACKAGE%"
+
+ECHO Making zip...
+7za.exe a source.zip %PROJECT_PATH%"/Assets" %PROJECT_PATH%"/ProjectSettings" %PROJECT_PATH%"/Packages"
+move source.zip %PACKAGE_PATH%
+cd /Users/danielmi/Documents/UnityWork/UnityRepos/arfoundation-demos/
+tar -cvzf source.tar.gz "Assets" "ProjectSettings" "Packages"
+move source.tar.gz %PACKAGE_PATH%
 PAUSE
