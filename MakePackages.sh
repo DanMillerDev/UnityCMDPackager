85045ab2b0371ea51ac39c90505c2cab69df9615
Dan <danielmi@unity3d.com>
2020-07-16 23:12:21 -0700

    updated for localization

--- MakePackages.sh
+++ MakePackages.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 echo "Making packages..."
-UNITY_PATH=/Applications/Unity/Hub/Editor/2019.3.0f6/Unity.app/Contents/MacOS/Unity
+UNITY_PATH=/Applications/Unity/Hub/Editor/2019.4.0f1/Unity.app/Contents/MacOS/Unity
 PROJECT_PATH=/Users/danielmi/Documents/AR_Repos/DemosMaster/arfoundation-demos
 PACKAGE_PATH=/Users/danielmi/Documents/AR_Repos/DemosMaster/ARFoundationDemosReleases/
 COMMON_FOLDER="Assets/Common"
 UX_FOLDER="Assets/UX"
 IMAGE_FOLDER="Assets/ImageTracking"
+ADDRESSABLES_FOLDER="Assets/AddressableAssetsData"
 UX_PACKAGE=UX.unitypackage
 IMAGE_PACKAGE=ImageTracking.unitypackage
 
-$UNITY_PATH -batchmode -nographics -quit -projectPath $PROJECT_PATH -exportPackage $COMMON_FOLDER $UX_FOLDER "$PACKAGE_PATH$UX_PACKAGE"
+$UNITY_PATH -batchmode -nographics -quit -projectPath $PROJECT_PATH -exportPackage $COMMON_FOLDER $UX_FOLDER $ADDRESSABLES_FOLDER "$PACKAGE_PATH$UX_PACKAGE"
 $UNITY_PATH -batchmode -nographics -quit -projectPath $PROJECT_PATH -exportPackage $COMMON_FOLDER $IMAGE_FOLDER "$PACKAGE_PATH$IMAGE_PACKAGE"
 
 echo "Making Zip..."
